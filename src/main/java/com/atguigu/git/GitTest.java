96a453e14ec955a88d10df7f4515aad1d639999d
xiangshulei <3029463907@qq.com>
2026-02-24 11:51:34 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test0");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
