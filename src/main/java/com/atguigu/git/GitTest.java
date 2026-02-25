67c6d47627a1c610dd093b1955d0dc8878224659
xiangshulei <16516819+xiangshulei@user.noreply.gitee.com>
2026-02-25 05:22:07 +0000

    update src/main/java/com/atguigu/git/GitTest.java.
    Signed-off-by: xiangshulei <16516819+xiangshulei@user.noreply.gitee.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gitee test");
+        System.out.println("gitee test2");
 
     }
 }
