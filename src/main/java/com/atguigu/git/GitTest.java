f95b41599326db4e13d24ea226c59abeaa0f45d3
xiangshlei <3029463907@qq.com>
2026-02-25 13:12:53 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot-fix test0");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
 
     }
 }
